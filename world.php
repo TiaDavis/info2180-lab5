e5d0e3f7b1f89b386a29bfc8266027192f783763
Tia Davis <tiajhaedavis258@gmail.com>
2021-11-08 22:44:30 -0500

    Made changes to php, css and js file

--- world.php
+++ world.php
@@ -1,17 +1,60 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$country =  filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$cityquery = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$context%'");
+$city = $cityquery->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<?php
+    header('Access-Control-Allow-Origin: *');
+?>
+
+<?php if(isset($country)&&(!isset($context))) :?>
+<table>
+          <thead>
+            <tr>
+              <th>Country</th>
+              <th>Continent</th>
+              <th>Year of Independence</th>
+              <th>Head of State</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($results as $row): ?>
+              <tr>
+                <td><?= $row['name']?></td>
+                <td><?= $row['continent']?></td>
+                <td><?= $row['independence_year']?></td>
+                <td><?= $row['head_of_state']?></td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+</table>
+<?php elseif (isset($context)): ?>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th> 
+    <th>Population</th>
+  </tr>
+  <?php foreach ($city as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+<?php endif ?>
+
